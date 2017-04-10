2723f8a5a8fe590d14f5e214c0a98d37e09eeeed
kerfendal yann <kerfendalyann@gmail.com>
2017-04-10 10:23:13 +0200

    ajout recherche

--- assets/js/appli.js
+++ assets/js/appli.js
@@ -261,11 +261,65 @@ function requeteAjax(){
 
 
 
+
+function mapSearch(){
+    var search = L.Control.extend({
+      onAdd: function() {
+        var element = document.createElement("input");
+
+        element.id = "searchBox";
+
+        return element;
+      }
+    });
+
+    (new search).addTo(objetMap);
+
+    var input = document.getElementById("searchBox");
+
+    var searchBox = new google.maps.places.SearchBox(input);
+
+    searchBox.addListener('places_changed', function() {
+      var places = searchBox.getPlaces();
+
+      if (places.length == 0) {
+        return;
+      }
+
+      var group = L.featureGroup();
+
+      places.forEach(function(place) {
+
+        // Create a marker for each place.
+        console.log(places);
+        console.log(place.geometry.location.lat() + " / " + place.geometry.location.lng());
+        var marker = L.marker([
+          place.geometry.location.lat(),
+          place.geometry.location.lng()
+        ]);
+        group.addLayer(marker);
+      });
+
+      group.addTo(objetMap);
+      objetMap.fitBounds(group.getBounds());
+
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
 initialiserCarte();
 //testLayer();
-
 requeteAjax();
-
 jQuery('#maLocation').click(function (e) {
     maLocalisation();
 });
+mapSearch();
