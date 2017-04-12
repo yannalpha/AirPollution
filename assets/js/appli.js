77c9b985007f82ca3f82101de91c50ed657394fa
c-delanneau <delanneau.charlie@gmail.com>
2017-04-12 14:21:21 +0200

    Requete ajax par type + au clic

--- assets/js/appli.js
+++ assets/js/appli.js
@@ -17,7 +17,18 @@ var maLng;
 
 // map and objetmap
 var objetMap;
-var layerMap;
+
+// nom des overlays
+var overlayCo2 = "Taux de Co2";
+var overlayPf = "Taux de micro particules";
+
+// booléens d'actiovation des overlays
+var isCo2Active = false;
+var isPfActive = false;
+
+// booléens de présence ou non de données dans les overlays
+var isCo2Empty = true;
+var isPfEmpty = true;
 
 // creation layer group heatmap vide
 var co2LayerGroup = L.layerGroup();
@@ -63,7 +74,7 @@ function initialiserCarte(){
         layers: [layerBase]
     });
 
-    layerMap = {
+    var layerMap = {
         "greymap": layerBase,
         "layer2": layer2,
         "layer3": layer3,
@@ -75,31 +86,80 @@ function initialiserCarte(){
     
     // initialisation d'overlay (filtre a cocher) via layer vide
     var overlayMaps = {
-        "Taux de Co2": co2LayerGroup,
-        "taux de micro particules": pfLayerGroup
+        [overlayCo2]: co2LayerGroup,
+        [overlayPf]: pfLayerGroup
     };
 
-    console.log(Object.keys(objetMap["_layers"]));
     L.control.layers(layerMap, overlayMaps).addTo(objetMap);
+
+    // action au event zoom and move
+    initActionEvents();
     
 };
 
 
 ///////////// La requette ajax
-function requeteAjax(requestUrl){
+function getParticulesFines() {
+    var requestUrl = 'http://cgportfolio.ddns.net/api/getBy' +
+                        '?lat=' + objetMap.getCenter().lat +
+                        '&long=' + objetMap.getCenter().lng +
+                        '&dLat=' + getDeltaLat() +
+                        '&dLong=' + getDeltaLong() + 
+                        '&type=PF';
     // rquette api
-    //http://10.40.73.234:8000/app.php/api/getAll
-    var data;
     $.ajax({
         url : requestUrl, // La ressource ciblée
         type : 'GET', // Le type de la requête HTTP.
-        crossDomain: true,
-    //    dataType: 'jsonp'
+        crossDomain: true
     }).done(function(data) {
-        //// on appel la fonction
-        console.log("mes datas :  ")
-        console.log(data);
+        console.log(data.length);
+        isPfEmpty = false;
         definitionDonnees(data);
+    }).fail(function(err) {
+        isPfEmpty = true;
+    });
+};
+
+///////////// La requette ajax
+function getCo2() {
+    var requestUrl = 'http://cgportfolio.ddns.net/api/getBy' +
+                        '?lat=' + objetMap.getCenter().lat +
+                        '&long=' + objetMap.getCenter().lng +
+                        '&dLat=' + getDeltaLat() +
+                        '&dLong=' + getDeltaLong() + 
+                        '&type=CO2';
+    // rquette api
+    $.ajax({
+        url : requestUrl, // La ressource ciblée
+        type : 'GET', // Le type de la requête HTTP.
+        crossDomain: true
+    }).done(function(data) {
+        console.log(data.length);
+        isCo2Empty = false;
+        definitionDonnees(data);
+    }).fail(function(err) {
+        isCo2Empty = true;
+    });
+};
+
+///////////// La requette ajax
+function getParticulesFinesAndCo2() {
+    var requestUrl = 'http://cgportfolio.ddns.net/api/getBy' +
+                        '?lat=' + objetMap.getCenter().lat +
+                        '&long=' + objetMap.getCenter().lng +
+                        '&dLat=' + getDeltaLat() +
+                        '&dLong=' + getDeltaLong();
+    // rquette api
+    $.ajax({
+        url : requestUrl, // La ressource ciblée
+        type : 'GET', // Le type de la requête HTTP.
+        crossDomain: true
+    }).done(function(data) {
+        console.log(data.length);
+        isPfEmpty = isCo2Empty = false;
+        definitionDonnees(data);
+    }).fail(function(err) {
+        isPfEmpty = isCo2Empty = true;
     });
 };
 
@@ -125,16 +185,22 @@ function definitionDonnees(data) {
 
     }
     
-    //Layer Groups
-    
-    // layer group class
-    
     co2LayerGroup.addLayer(L.heatLayer(co2Points, {blur: 30}));
     pfLayerGroup.addLayer(L.heatLayer(pfPoints, {blur: 25}));
     
-    
-    
 };
+
+function getDeltaLat() {
+    var lattitudeNordEst = objetMap.getBounds()._northEast.lat;
+    var lattitudeSouthWest = objetMap.getBounds()._southWest.lat;
+    return (lattitudeNordEst-lattitudeSouthWest)/2;
+}
+
+function getDeltaLong() {
+    var longitudeNordEst = objetMap.getBounds()._northEast.lng;
+    var longitudeSouthWest = objetMap.getBounds()._southWest.lng;
+    return (longitudeNordEst-longitudeSouthWest)/2;
+}
 
 
 /// la fonction recherche
@@ -184,38 +250,45 @@ function mapSearch(){
 
 
 //Zoom & move event, on genere le delta lat et long
-function actionEvent(action){
-    objetMap.on(action, function(){
-        zoomActuel = objetMap.getZoom();
-        
-        var centerMapLat = objetMap.getCenter().lat;
-        var centerMapLong = objetMap.getCenter().lng;
+function initActionEvents(){
+    objetMap.on('overlayadd', function(e) {
+        if (e.name && e.name == overlayPf) {
+            isPfActive = true;
+            if (isPfEmpty)
+                getParticulesFines();
+        }
+        else if (e.name && e.name == overlayCo2) {
+            isCo2Active = true;
+            if (isCo2Empty)
+                getCo2();
+        }
+    }).on('overlayremove', function(e) {
+        if (e.name && e.name == overlayPf)
+            isPfActive = false;
+        else if (e.name && e.name == overlayCo2)
+            isCo2Active = false;
+    }).on("moveend", function(){
 
-        var lattitudeNordEst = objetMap.getBounds()["_northEast"]["lat"];
-        var longitudeNordEst = objetMap.getBounds()["_northEast"]["lng"];
+        if (!isCo2Empty) {
+            co2LayerGroup.eachLayer(function(layer){
+                co2LayerGroup.removeLayer(layer);
+            });
+            isCo2Empty = true;
+        }
 
-        var lattitudeSouthWest = objetMap.getBounds()["_southWest"]["lat"];
-        var longitudeSouthWest = objetMap.getBounds()["_southWest"]["lng"];
-
-        var dLat = (lattitudeNordEst - lattitudeSouthWest)/2 ;
-
-        var dLong = (longitudeNordEst - longitudeSouthWest)/2;
+        if (!isPfEmpty) {
+            pfLayerGroup.eachLayer(function(layer){
+                pfLayerGroup.removeLayer(layer);
+            });
+            isPfEmpty = true;
+        }
         
-        co2LayerGroup.eachLayer(function(layer){
-            co2LayerGroup.removeLayer(layer);
-        });
-        
-        pfLayerGroup.eachLayer(function(layer){
-            pfLayerGroup.removeLayer(layer);
-        });
-        
-        var requestUrl = 'http://cgportfolio.ddns.net/api/getBy' +
-                        '?lat=' + centerMapLat +
-                        '&long=' + centerMapLong +
-                        '&dLat=' + dLat +
-                        '&dLong=' + dLong;
-        
-        requeteAjax(requestUrl);
+        if (isCo2Active && isPfActive)
+            getParticulesFinesAndCo2();
+        else if (isCo2Active)
+            getCo2();
+        else if (isPfActive)
+            getParticulesFines();
 
     });
 };
@@ -228,30 +301,26 @@ initialiserCarte();
 // la fonction de recherche
 mapSearch();
 
-// action au event zoom and move
-actionEvent("moveend");
-
 // initialisation des datepickers
 jQuery(function() {
     jQuery(".datepicker-debut").datepicker();
     jQuery(".datepicker-fin").datepicker();
-});
-
-// au clic sur "me localiser"
-jQuery('#maLocation').click(function(e) {
-    // localisation 
-    if(navigator.geolocation)
-    {
-        navigator.geolocation.getCurrentPosition(function(position){
-                maLat = position.coords.latitude;
-                maLng = position.coords.longitude;
-                objetMap.flyTo([maLat,maLng], 10);
-                var maLocalisation = L.marker([maLat, maLng]).addTo(objetMap).bindPopup('Vous ètes ici !').openPopup();
-                console.log('localisation trouvée, vous etes chanceux!');
-        });
-    }
-    else
-        alert('localisation non accessible');
+    // au clic sur "me localiser"
+    jQuery('#maLocation').click(function(e) {
+        // localisation 
+        if(navigator.geolocation)
+        {
+            navigator.geolocation.getCurrentPosition(function(position){
+                    maLat = position.coords.latitude;
+                    maLng = position.coords.longitude;
+                    objetMap.flyTo([maLat,maLng], 10);
+                    var maLocalisation = L.marker([maLat, maLng]).addTo(objetMap).bindPopup('Vous ètes ici !').openPopup();
+                    console.log('localisation trouvée, vous etes chanceux!');
+            });
+        }
+        else
+            alert('localisation non accessible');
+    });
 });
 
 
